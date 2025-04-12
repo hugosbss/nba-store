e862befce4acffd4080e4d6210e9db11e6794ef6
Hugo <hiago.hizu@gmail.com>
2025-04-12 14:02:43 -0300

    Renomeando a pasta aula14 para A14

--- A10/php/6.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //6. Um formulário que recebe um valor em metros e converte para centímetros utilizando o método GET.
-    if (isset($_GET['metros'])) {
-        $metros = $_GET['metros'];
-        $centimetros = $metros * 100;
-        echo "$metros metros é igual a $centimetros centímetros.";
-    }
-    echo "<br><hr />";
-?>
--- A10/php/9.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //9. Um formulário que recebe o raio de um círculo e calcula a área utilizando o método POST. Fórmula: Área = π * raio²
-    if (isset($_POST['raio'])) {
-        $raio = $_POST['raio'];
-        $area = M_PI * pow($raio, 2);
-        echo "A área do círculo é: $area";
-    }
-    echo "<br><hr />";
-?>

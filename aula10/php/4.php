4653f5f9f6088e5afe09d747a669f5ddee5b71ae
Hugo <hiago.hizu@gmail.com>
2025-04-12 14:00:28 -0300

    Removendo pastas erradas

--- aula10/php/4.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //4. Um formulário que recebe uma temperatura em Fahrenheit e converte para Celsius utilizando o método GET. Fórmula: C = (F - 32) * 5/9 
-    if (isset($_GET['fahrenheit'])) {
-        $fahrenheit = $_GET['fahrenheit'];
-        $celsius = ($fahrenheit - 32) * 5 / 9;
-        echo "$fahrenheit °F é igual a $celsius °C.";
-    }
-    echo "<br><hr />";
-?>
